dd0ce6a7fb56bb0ee5cf7efd9306887886e045ef
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2024-12-11 17:32:00 +0530

    Update juiceshop.tf

--- juiceshop.tf
+++ juiceshop.tf
@@ -1,5 +1,8 @@
 provider "azurerm" {
-  features {}
+  subscription_id = var.SUBSCRIPTION_ID
+  client_id       = var.CLIENT_ID
+  client_secret   = var.CLIENT_SECRET
+  tenant_id       = var.TENANT_ID
 }
 
 # New Resource Group
