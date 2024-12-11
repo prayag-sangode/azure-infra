c7082689d1a7c5136e283682bae8827105e1abd8
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2024-12-11 17:28:48 +0530

    Update juiceshop.tf

--- juiceshop.tf
+++ juiceshop.tf
@@ -1,53 +1,48 @@
-# Provider Configuration
 provider "azurerm" {
   features {}
-
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
 }
 
-# 1. Resource Group
-resource "azurerm_resource_group" "juice_shop_rg" {
-  name     = "juice-shop-rg-new"
-  location = "East US"
+# New Resource Group
+resource "azurerm_resource_group" "juice_shop_new_rg" {
+  name     = "juice-shop-new-rg"  # New Resource Group Name
+  location = "East US"            # Location for the resource group
 }
 
-# 2. Virtual Network and Subnet
+# Virtual Network
 resource "azurerm_virtual_network" "juice_shop_vnet" {
   name                = "juice-shop-vnet"
+  location            = azurerm_resource_group.juice_shop_new_rg.location
+  resource_group_name = azurerm_resource_group.juice_shop_new_rg.name
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.juice_shop_rg.location
-  resource_group_name = azurerm_resource_group.juice_shop_rg.name
 }
 
+# Subnet
 resource "azurerm_subnet" "juice_shop_subnet" {
   name                 = "juice-shop-subnet"
-  resource_group_name  = azurerm_resource_group.juice_shop_rg.name
+  resource_group_name  = azurerm_resource_group.juice_shop_new_rg.name
   virtual_network_name = azurerm_virtual_network.juice_shop_vnet.name
-  address_prefixes     = ["10.0.0.0/24"]
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
-# 3. Network Interface
+# Network Interface (for container group)
 resource "azurerm_network_interface" "juice_shop_nic" {
   name                = "juice-shop-nic"
-  location            = azurerm_resource_group.juice_shop_rg.location
-  resource_group_name = azurerm_resource_group.juice_shop_rg.name
+  location            = azurerm_resource_group.juice_shop_new_rg.location
+  resource_group_name = azurerm_resource_group.juice_shop_new_rg.name
 
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.juice_shop_subnet.id
     private_ip_address_allocation = "Static"
-    private_ip_address            = "10.0.0.4"  # Static IP Address
+    private_ip_address            = "10.0.1.4"  # Static private IP for the container
   }
 }
 
-# 4. Azure Container Instance
+# Container Group (with no public IP)
 resource "azurerm_container_group" "juice_shop_container" {
   name                = "juice-shop-container"
-  location            = azurerm_resource_group.juice_shop_rg.location
-  resource_group_name = azurerm_resource_group.juice_shop_rg.name
+  location            = azurerm_resource_group.juice_shop_new_rg.location
+  resource_group_name = azurerm_resource_group.juice_shop_new_rg.name
   os_type             = "Linux"
   tags                = {
     environment = "production"
@@ -68,6 +63,14 @@ resource "azurerm_container_group" "juice_shop_container" {
     }
   }
 
-  # Corrected usage of subnet_ids instead of network_profile_id
+  # Subnet to use internal IP
   subnet_ids = [azurerm_subnet.juice_shop_subnet.id]
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.juice_shop_new_rg.name
+}
+
+output "container_group_name" {
+  value = azurerm_container_group.juice_shop_container.name
 }
