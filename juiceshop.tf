474aa6be3e8e2eb5886a35d834d956bede0f3c63
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2024-12-11 23:04:12 +0530

    Update juiceshop.tf

--- juiceshop.tf
+++ juiceshop.tf
@@ -1,54 +1,127 @@
-name: Terraform Deployment
+# Resource Group
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
 
-on:
-  push:
-    branches:
-      - main
-  pull_request:
-    branches:
-      - main
+resource "azurerm_resource_group" "rg" {
+  name     = random_pet.rg_name.id
+  location = var.resource_group_location
+}
 
-jobs:
-  terraform:
-    name: Deploy Terraform Resources
-    runs-on: ubuntu-latest
+# Virtual Network and Subnet with Delegation
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-${random_pet.rg_name.id}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
 
-    steps:
-      # Checkout the repository
-      - name: Checkout Code
-        uses: actions/checkout@v3
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-${random_pet.rg_name.id}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 
-      # Setup Terraform
-      - name: Setup Terraform
-        uses: hashicorp/setup-terraform@v2
+  delegation {
+    name = "aci-delegation"
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
+  }
+}
 
-      # Configure Azure Authentication and Export Variables
-      - name: Authenticate to Azure
-        run: |
-          echo "Setting up Azure credentials..."
-          export ARM_CLIENT_ID=${{ secrets.ARM_CLIENT_ID }}
-          export ARM_CLIENT_SECRET=${{ secrets.ARM_CLIENT_SECRET }}
-          export ARM_SUBSCRIPTION_ID=${{ secrets.ARM_SUBSCRIPTION_ID }}
-          export ARM_TENANT_ID=${{ secrets.ARM_TENANT_ID }}
+# Container Group
+resource "random_string" "container_name" {
+  length  = 25
+  lower   = true
+  upper   = false
+  special = false
+}
 
-      # Terraform Init
-      - name: Terraform Init
-        run: terraform init
+resource "azurerm_container_group" "container" {
+  name                = "${var.container_group_name_prefix}-${random_string.container_name.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  restart_policy      = var.restart_policy
+  subnet_ids          = [azurerm_subnet.subnet.id]  # Subnet for private IP
+  ip_address_type     = "Private"                  # Must use private IP for subnet
 
-      # Terraform Plan
-      - name: Terraform Plan
-        env:
-          TF_VAR_subscription_id: ${{ secrets.ARM_SUBSCRIPTION_ID }}
-          TF_VAR_tenant_id: ${{ secrets.ARM_TENANT_ID }}
-          TF_VAR_client_id: ${{ secrets.ARM_CLIENT_ID }}
-          TF_VAR_client_secret: ${{ secrets.ARM_CLIENT_SECRET }}
-        run: terraform plan
+  container {
+    name   = "${var.container_name_prefix}-${random_string.container_name.result}"
+    image  = var.image
+    cpu    = var.cpu_cores
+    memory = var.memory_in_gb
 
-      # Terraform Apply
-      - name: Terraform Apply
-        env:
-          TF_VAR_subscription_id: ${{ secrets.ARM_SUBSCRIPTION_ID }}
-          TF_VAR_tenant_id: ${{ secrets.ARM_TENANT_ID }}
-          TF_VAR_client_id: ${{ secrets.ARM_CLIENT_ID }}
-          TF_VAR_client_secret: ${{ secrets.ARM_CLIENT_SECRET }}
-        run: terraform destroy --auto-approve
+    ports {
+      port     = var.port
+      protocol = "TCP"
+    }
+  }
+}
+
+# Variables
+variable "resource_group_location" {
+  type        = string
+  default     = "eastus"
+  description = "Location for all resources."
+}
+
+variable "resource_group_name_prefix" {
+  type        = string
+  default     = "rg"
+  description = "Prefix of the resource group name that's combined with a random value so name is unique in your Azure subscription."
+}
+
+variable "container_group_name_prefix" {
+  type        = string
+  default     = "acigroup"
+  description = "Prefix of the container group name that's combined with a random value so name is unique in your Azure subscription."
+}
+
+variable "container_name_prefix" {
+  type        = string
+  default     = "aci"
+  description = "Prefix of the container name that's combined with a random value so name is unique in your Azure subscription."
+}
+
+# Update the container image to use Juiceshop
+variable "image" {
+  type        = string
+  default     = "bkimminich/juice-shop"  # Updated image for Juiceshop
+  description = "Container image to deploy. Should be of the form repoName/imagename:tag for images stored in public Docker Hub, or a fully qualified URI for other registries. Images from private registries require additional registry credentials."
+}
+
+variable "port" {
+  type        = number
+  default     = 80
+  description = "Port to open on the container."
+}
+
+variable "cpu_cores" {
+  type        = number
+  default     = 1
+  description = "The number of CPU cores to allocate to the container."
+}
+
+variable "memory_in_gb" {
+  type        = number
+  default     = 2
+  description = "The amount of memory to allocate to the container in gigabytes."
+}
+
+variable "restart_policy" {
+  type        = string
+  default     = "Always"
+  description = "The behavior of Azure runtime if container has stopped."
+  validation {
+    condition     = contains(["Always", "Never", "OnFailure"], var.restart_policy)
+    error_message = "The restart_policy must be one of the following: Always, Never, OnFailure."
+  }
+}
+
+# Outputs
+output "container_ipv4_address" {
+  value = azurerm_container_group.container.ip_address
+}
