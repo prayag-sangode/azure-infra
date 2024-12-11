176196e0cf412f0af84c10811df3157ee8cce6d6
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2024-12-12 00:24:35 +0530

    Update appgw.tf

--- appgw.tf
+++ appgw.tf
@@ -1,60 +1,55 @@
-# Application Gateway
 resource "azurerm_application_gateway" "app_gateway" {
   name                = "myAppGateway"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   sku {
     name     = "Standard_v2"
+    tier     = "Standard_v2"
     capacity = 2
   }
-
   frontend_ip_configuration {
-    name                                     = "frontendConfig"
-    public_ip_address_id                    = azurerm_public_ip.public_ip.id
+    name                 = "frontend-config"
+    public_ip_address_id = azurerm_public_ip.my_public_ip.id
   }
 
   backend_address_pool {
-    name = "backendPool"
-
+    name = "backend-pool"
     backend_addresses {
-      ip_address = var.aci_ip  # This will be the IP of your container group
+      ip_address = output.container_ipv4_address.value # Using the output here
     }
   }
 
   http_settings {
-    name                               = "httpSettings"
-    port                               = 80
-    protocol                           = "Http"
-    cookie_based_affinity              = "Disabled"
+    name                           = "http-settings"
+    port                           = 80
+    protocol                       = "Http"
+    cookie_based_affinity          = "Disabled"
     request_timeout {
       seconds = 20
     }
   }
 
   listener {
-    name                                 = "listener"
-    frontend_ip_configuration_id       = azurerm_application_gateway_frontend_ip_configuration.frontend_config.id
-    frontend_port {
-      port = 80
-    }
+    name                                 = "app-gateway-listener"
+    frontend_ip_configuration_id        = azurerm_application_gateway_frontend_ip_configuration.my_frontend_ip.id
+    frontend_port_id                    = azurerm_application_gateway_frontend_port.my_frontend_port.id
     protocol                             = "Http"
+    ssl_certificate_id                  = null
   }
 
-  routing_rule {
-    name                       = "rule1"
-    priority                   = 100
-    rule_type                  = "Basic"
-    backend_address_pool_id    = azurerm_application_gateway_backend_address_pool.backend_pool.id
-    http_settings_id           = azurerm_application_gateway_http_settings.http_settings.id
+  gateway_ip_configuration {
+    name                 = "app-gateway-vnet"
+    subnet_id            = azurerm_subnet.subnet.id
   }
 
-  tags = {
-    environment = "production"
+  url_path_map {
+    default_backend_address_pool_id = azurerm_application_gateway_backend_address_pool.backend_pool.id
+    default_backend_http_settings_id = azurerm_application_gateway_http_settings.http_settings.id
+    default_backend_address_pool {
+      backend_addresses {
+        ip_address = output.container_ipv4_address.value
+      }
+    }
   }
 }
 
-# Variables
-variable "aci_ip" {
-  type        = string
-  description = "IP address of the backend container group"
-}
