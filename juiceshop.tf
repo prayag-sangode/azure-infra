c6285baaeaedf5d859149cb1fb6ae027f7da8bec
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2024-12-11 18:27:10 +0530

    Update juiceshop.tf

--- juiceshop.tf
+++ juiceshop.tf
@@ -1,26 +1,3 @@
-resource "random_pet" "rg_name" {
-  prefix = var.resource_group_name_prefix
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = random_pet.rg_name.id
-  location = var.resource_group_location
-}
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-${random_pet.rg_name.id}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"]
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet-${random_pet.rg_name.id}"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
 resource "random_string" "container_name" {
   length  = 25
   lower   = true
