e83f3fa9f9e27805c469ae7b46dfe723f19eec26
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2024-12-11 23:48:22 +0530

    Update app-gw.tf

--- app-gw.tf
+++ app-gw.tf
@@ -1,17 +1,74 @@
-# Application Gateway Subnet
-resource "azurerm_subnet" "appgw_subnet" {
-  name                 = "appgw-subnet-${random_pet.rg_name.id}"
+# Resource Group
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = random_pet.rg_name.id
+  location = var.resource_group_location
+}
+
+# Virtual Network and Subnet with Delegation
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-${random_pet.rg_name.id}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-${random_pet.rg_name.id}"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"] # Ensure this does not overlap with other subnets
+  address_prefixes     = ["10.0.1.0/24"]
+
+  delegation {
+    name = "aci-delegation"
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
+  }
 }
+
+# Container Group
+resource "random_string" "container_name" {
+  length  = 25
+  lower   = true
+  upper   = false
+  special = false
+}
+
+resource "azurerm_container_group" "container" {
+  name                = "${var.container_group_name_prefix}-${random_string.container_name.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  restart_policy      = var.restart_policy
+  subnet_ids          = [azurerm_subnet.subnet.id]  # Subnet for private IP
+  ip_address_type     = "Private"                  # Must use private IP for subnet
+
+  container {
+    name   = "${var.container_name_prefix}-${random_string.container_name.result}"
+    image  = var.image
+    cpu    = var.cpu_cores
+    memory = var.memory_in_gb
+
+    ports {
+      port     = var.port
+      protocol = "TCP"
+    }
+  }
+}
+
+# Application Gateway Configuration
 
 # Public IP for Application Gateway
 resource "azurerm_public_ip" "appgw_pip" {
-  name                = "appgw-pip-${random_pet.rg_name.id}"
-  location            = azurerm_resource_group.rg.location
+  name                = "appgw-public-ip"
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
 }
 
 # Application Gateway
@@ -23,63 +80,61 @@ resource "azurerm_application_gateway" "appgw" {
   sku {
     name     = "Standard_v2"
     tier     = "Standard_v2"
-    capacity = 1
+    capacity = 2
   }
 
   gateway_ip_configuration {
-    name      = "appgw-ipconfig"
-    subnet_id = azurerm_subnet.appgw_subnet.id
-  }
-
-  frontend_port {
-    name = "frontend-port"
-    port = 80
+    name      = "appgw-gateway-ip"
+    subnet_id = azurerm_subnet.subnet.id
   }
 
   frontend_ip_configuration {
-    name                 = "appgw-frontend"
+    name                 = "frontend-ip-appgw"
     public_ip_address_id = azurerm_public_ip.appgw_pip.id
   }
 
-  backend_address_pool {
-    name = "aci-backend-pool"
+  frontend_port {
+    name = "frontend-port-80"
+    port = 80
+  }
 
-    # Use ACI's private IP here
+  backend_address_pool {
+    name = "appgw-backend-pool"
     backend_addresses {
       ip_address = azurerm_container_group.container.ip_address
     }
   }
 
   backend_http_settings {
-    name                  = "backend-http-settings"
+    name                  = "appgw-http-settings"
     cookie_based_affinity = "Disabled"
-    port                  = var.port
+    port                  = 80
     protocol              = "Http"
-    request_timeout       = 20
   }
 
   http_listener {
-    name                           = "http-listener"
-    frontend_ip_configuration_name = "appgw-frontend"
-    frontend_port_name             = "frontend-port"
+    name                           = "appgw-http-listener"
+    frontend_ip_configuration_name = "frontend-ip-appgw"
+    frontend_port_name             = "frontend-port-80"
     protocol                       = "Http"
   }
 
   request_routing_rule {
-    name                       = "routing-rule"
+    name                       = "route-to-backend"
+    priority                   = 10
     rule_type                  = "Basic"
-    http_listener_name         = "http-listener"
-    backend_address_pool_name  = "aci-backend-pool"
-    backend_http_settings_name = "backend-http-settings"
-  }
-
-  tags = {
-    Environment = "Dev"
+    http_listener_name         = "appgw-http-listener"
+    backend_address_pool_name  = "appgw-backend-pool"
+    backend_http_settings_name = "appgw-http-settings"
   }
 }
 
-# Output Public IP of Application Gateway
+# Outputs
+output "container_ipv4_address" {
+  value = azurerm_container_group.container.ip_address
+}
+
 output "appgw_public_ip" {
-  value       = azurerm_public_ip.appgw_pip.ip_address
-  description = "Public IP address of the Application Gateway"
+  description = "Public IP Address of Application Gateway"
+  value       = azurerm_application_gateway.appgw.frontend_ip_configuration[0].public_ip_address_id
 }
