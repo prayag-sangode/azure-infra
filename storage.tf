668766933ea3ed3e978f33f55762fd715a22492f
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2024-12-11 17:19:58 +0530

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -1,11 +1,11 @@
-provider "azurerm" {
-  features {}
-
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-}
+#provider "azurerm" {
+#  features {}
+#
+#  subscription_id = var.subscription_id
+#  tenant_id       = var.tenant_id
+#  client_id       = var.client_id
+#  client_secret   = var.client_secret
+#}
 
 # Variables for authentication
 variable "subscription_id" {
