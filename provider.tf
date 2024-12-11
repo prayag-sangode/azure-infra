ee9059b86681b262a994f0b1874a755f7fd5b8d8
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2024-12-11 18:02:47 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,29 @@
+provider "azurerm" {
+  features {}
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+}
+
+# Declare input variables for Azure authentication
+variable "client_id" {
+  description = "Azure Client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
