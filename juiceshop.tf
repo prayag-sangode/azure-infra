1dff04246fa1f587482c533e936bc5cda238166f
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2024-12-11 18:24:09 +0530

    Update juiceshop.tf

--- juiceshop.tf
+++ juiceshop.tf
@@ -1,4 +1,21 @@
-# Create a Virtual Network
+provider "azurerm" {
+  features {}
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+}
+
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = random_pet.rg_name.id
+  location = var.resource_group_location
+}
+
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-${random_pet.rg_name.id}"
   location            = azurerm_resource_group.rg.location
@@ -6,7 +23,6 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
 }
 
-# Create a Subnet
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet-${random_pet.rg_name.id}"
   resource_group_name  = azurerm_resource_group.rg.name
@@ -14,16 +30,23 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-# Updated Container Group
+resource "random_string" "container_name" {
+  length  = 25
+  lower   = true
+  upper   = false
+  special = false
+}
+
 resource "azurerm_container_group" "container" {
   name                = "${var.container_group_name_prefix}-${random_string.container_name.result}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  ip_address_type     = "Private"
   os_type             = "Linux"
   restart_policy      = var.restart_policy
 
-  subnet_ids = [azurerm_subnet.subnet.id] # Assign the subnet to the container group
+  network_profile {
+    id = azurerm_subnet.subnet.id
+  }
 
   container {
     name   = "${var.container_name_prefix}-${random_string.container_name.result}"
@@ -36,6 +59,84 @@ resource "azurerm_container_group" "container" {
       protocol = "TCP"
     }
   }
+}
+
+variable "resource_group_location" {
+  type        = string
+  default     = "eastus"
+  description = "Location for all resources."
+}
+
+variable "resource_group_name_prefix" {
+  type        = string
+  default     = "rg"
+  description = "Prefix for the resource group name."
+}
+
+variable "container_group_name_prefix" {
+  type        = string
+  default     = "acigroup"
+  description = "Prefix for the container group name."
+}
+
+variable "container_name_prefix" {
+  type        = string
+  default     = "aci"
+  description = "Prefix for the container name."
+}
+
+variable "image" {
+  type        = string
+  default     = "mcr.microsoft.com/azuredocs/aci-helloworld"
+  description = "Container image to deploy."
+}
+
+variable "port" {
+  type        = number
+  default     = 80
+  description = "Port to open on the container."
+}
+
+variable "cpu_cores" {
+  type        = number
+  default     = 1
+  description = "CPU cores to allocate to the container."
+}
+
+variable "memory_in_gb" {
+  type        = number
+  default     = 2
+  description = "Memory to allocate to the container (in GB)."
+}
+
+variable "restart_policy" {
+  type        = string
+  default     = "Always"
+  description = "Container restart policy."
+  validation {
+    condition     = contains(["Always", "Never", "OnFailure"], var.restart_policy)
+    error_message = "Restart policy must be one of: Always, Never, OnFailure."
+  }
+}
+
+variable "client_id" {
+  type        = string
+  description = "Azure client ID."
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Azure client secret."
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "Azure subscription ID."
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure tenant ID."
 }
 
 output "container_ipv4_address" {
