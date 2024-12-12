cc4b7d978e7e173b040e257ba5c35863bd64dbcb
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2024-12-12 09:00:42 +0530

    Update juiceshop.tf

--- juiceshop.tf
+++ juiceshop.tf
@@ -1,24 +1,14 @@
-# Resource Group
-resource "random_pet" "rg_name" {
-  prefix = var.resource_group_name_prefix
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = random_pet.rg_name.id
-  location = var.resource_group_location
-}
-
 # Virtual Network and Subnet with Delegation
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-${random_pet.rg_name.id}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = "westus"
+  resource_group_name = "juiceshop-rg"
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet-${random_pet.rg_name.id}"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = "juiceshop-rg"
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 
@@ -40,84 +30,25 @@ resource "random_string" "container_name" {
 }
 
 resource "azurerm_container_group" "container" {
-  name                = "${var.container_group_name_prefix}-${random_string.container_name.result}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  name                = "acigroup-${random_string.container_name.result}"
+  location            = "westus"
+  resource_group_name = "juiceshop-rg"
   os_type             = "Linux"
-  restart_policy      = var.restart_policy
+  restart_policy      = "Always"
   subnet_ids          = [azurerm_subnet.subnet.id]  # Subnet for private IP
   ip_address_type     = "Private"                  # Must use private IP for subnet
 
   container {
-    name   = "${var.container_name_prefix}-${random_string.container_name.result}"
-    image  = var.image
-    cpu    = var.cpu_cores
-    memory = var.memory_in_gb
+    name   = "juiceshop-container101"
+    image  = "bkimminich/juice-shop:latest" 
+    #image = "mcr.microsoft.com/azuredocs/aci-helloworld"
+    cpu    = "1"
+    memory = "2"
 
     ports {
-      port     = var.port
+      port     = 80
       protocol = "TCP"
     }
-  }
-}
-
-# Variables
-variable "resource_group_location" {
-  type        = string
-  default     = "westus"
-  description = "Location for all resources."
-}
-
-variable "resource_group_name_prefix" {
-  type        = string
-  default     = "rg"
-  description = "Prefix of the resource group name that's combined with a random value so name is unique in your Azure subscription."
-}
-
-variable "container_group_name_prefix" {
-  type        = string
-  default     = "acigroup"
-  description = "Prefix of the container group name that's combined with a random value so name is unique in your Azure subscription."
-}
-
-variable "container_name_prefix" {
-  type        = string
-  default     = "aci"
-  description = "Prefix of the container name that's combined with a random value so name is unique in your Azure subscription."
-}
-
-variable "image" {
-  type        = string
-  default     = "mcr.microsoft.com/azuredocs/aci-helloworld" 
-  #default     = "bkimminich/juice-shop:latest"
-  description = "Container image to deploy. Should be of the form repoName/imagename:tag for images stored in public Docker Hub, or a fully qualified URI for other registries. Images from private registries require additional registry credentials."
-}
-
-variable "port" {
-  type        = number
-  default     = 80
-  description = "Port to open on the container."
-}
-
-variable "cpu_cores" {
-  type        = number
-  default     = 1
-  description = "The number of CPU cores to allocate to the container."
-}
-
-variable "memory_in_gb" {
-  type        = number
-  default     = 2
-  description = "The amount of memory to allocate to the container in gigabytes."
-}
-
-variable "restart_policy" {
-  type        = string
-  default     = "Always"
-  description = "The behavior of Azure runtime if container has stopped."
-  validation {
-    condition     = contains(["Always", "Never", "OnFailure"], var.restart_policy)
-    error_message = "The restart_policy must be one of the following: Always, Never, OnFailure."
   }
 }
 
